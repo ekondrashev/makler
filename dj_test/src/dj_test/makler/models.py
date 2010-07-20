4d847176aca1bdb0d5ca1c2bcfd15b388f7b5f8a
Eugene <eugene@eugene-laptop.(none)>
2010-07-20 22:25:17 +0300

    Updated model: added Makler and PhoneNumber tables. Added static getUnknownStreet and getUnknownMakler methods to access some stub entries when no such info specified in adv.

--- dj_test/src/dj_test/makler/models.py
+++ dj_test/src/dj_test/makler/models.py
@@ -1,15 +1,25 @@
 from django.db import models
 
 class Street(models.Model):
+    UNKNOWN_STREET_NAME='UNKNOWN'
     name = models.CharField(max_length=50)
+    @staticmethod
+    def getUnknownStreet():
+        try:
+            unknown = Street.objects.get(name=Street.UNKNOWN_STREET_NAME)
+        except Street.DoesNotExist:
+            unknown = Street(name=Street.UNKNOWN_STREET_NAME)
+            unknown.save()
+        return unknown
+
 
 class StreetFreak(models.Model):
     street = models.ForeignKey(Street)
     name = models.CharField(max_length=50)
 
 class Address(models.Model):
-    street = models.ForeignKey(Street, related_name = "street")
-    corner_street = models.ForeignKey(Street, related_name = "corner_street")
+    street = models.ForeignKey(Street, related_name = "street", default=Street.getUnknownStreet())
+    corner_street = models.ForeignKey(Street, related_name = "corner_street", default=Street.getUnknownStreet())
     number = models.CharField(max_length=10)
 
 class Flat(models.Model):
@@ -32,3 +42,21 @@ class Lease(models.Model):
     price = models.FloatField()
     currency = models.CharField(max_length=10)
     period = models.CharField(max_length=50)
+
+class Makler(models.Model):
+    name = models.CharField(max_length=20)
+    UNKNOWN_MAKLER_NAME = 'UNKNOWN'
+    @staticmethod
+    def getUnknownMakler():
+        try:
+            unknown = Makler.objects.get(name=Makler.UNKNOWN_MAKLER_NAME)
+        except Makler.DoesNotExist:
+            unknown = Makler(name=Makler.UNKNOWN_MAKLER_NAME)
+            unknown.save()
+        return unknown
+        
+class PhoneNumber(models.Model):
+    number = models.CharField(max_length=10)
+    lease = models.ForeignKey(Lease)
+    makler = models.ForeignKey(Makler, default=Makler.getUnknownMakler())
+    
